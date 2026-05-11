900f96a6387a67b554c7bfe4b130fcc0a86a35b4
James Cameron <jamiecam43@gmail.com>
2026-05-11 14:34:52 -0400

    completed quiz functionality including reset and highscore

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,133 @@
+import { useEffect, useReducer } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
+import Loader from './components/Loader';
+import Error from './components/Error';
+import StartScreen from './components/StartScreen';
+import Question from './components/Question';
+import NextButton from './components/NextButton';
+import Progress from './components/Progress';
+import FinishScreen from './components/FinishScreen';
+
+const initialState = {
+	questions: [],
+	points: 0,
+	status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished',
+	index: 0,
+	answer: null,
+	highscore: 0,
+};
+function reducer(state, action) {
+	switch (action.type) {
+		case 'dataReceived':
+			return { ...state, questions: action.payload, status: 'ready' };
+
+		case 'dataFailed':
+			return { ...state, status: 'error' };
+
+		case 'start':
+			return { ...state, status: 'active' };
+		case 'finished':
+			return {
+				...state,
+				status: 'finished',
+				highscore:
+					state.points > state.highscore ? state.points : state.highscore,
+			};
+
+		case 'newAnswer':
+			const question = state.questions.at(state.index);
+			return {
+				...state,
+				answer: action.payload,
+				points:
+					action.payload === question.correctOption
+						? state.points + question.points
+						: state.points,
+			};
+
+		case 'nextQuestion':
+			return {
+				...state,
+				index: state.index++,
+				answer: null,
+			};
+		case 'restart':
+			return {
+				...initialState,
+				questions: state.questions,
+				highscore: state.highscore,
+				status: 'ready',
+			};
+		default:
+			return state;
+	}
+}
 
 function App() {
+	const [{ status, questions, points, index, answer, highscore }, dispatch] =
+		useReducer(reducer, initialState);
+
+	const numQuestions = questions.length;
+	const maxPoints = questions
+		.map((q) => q.points)
+		.reduce((prev, cur) => prev + cur, 0);
+
+	useEffect(function () {
+		async function fetchQuestions() {
+			try {
+				const res = await fetch('http://localhost:8000/questions');
+				const data = await res.json();
+
+				dispatch({ type: 'dataReceived', payload: data });
+			} catch (err) {
+				dispatch({ type: 'dataFailed' });
+			}
+		}
+		fetchQuestions();
+	}, []);
 	return (
 		<div className='app'>
 			<Header />
+			<Main>
+				{status === 'loading' && <Loader />}
+				{status === 'error' && <Error />}
+				{status === 'ready' && (
+					<StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+				)}
+				{status === 'active' && (
+					<>
+						<Progress
+							index={index}
+							numQuestions={numQuestions}
+							points={points}
+							maxPoints={maxPoints}
+							answer={answer}
+						/>
+						<Question
+							question={questions[index]}
+							dispatch={dispatch}
+							answer={answer}
+						/>
+						<NextButton
+							dispatch={dispatch}
+							answer={answer}
+							index={index}
+							numQuestions={numQuestions}
+						/>
+					</>
+				)}
+				{status === 'finished' && (
+					<FinishScreen
+						dispatch={dispatch}
+						points={points}
+						maxPoints={maxPoints}
+						highscore={highscore}>
+						You scored
+					</FinishScreen>
+				)}
+			</Main>
 		</div>
 	);
 }
---
+++ src/components/FinishScreen.jsx
@@ -0,0 +1,29 @@
+export default function FinishScreen({
+	dispatch,
+	points,
+	maxPoints,
+	highscore,
+}) {
+	const percentage = (points / maxPoints) * 100;
+	let emoji;
+	if (percentage === 100) emoji = '🥇';
+	if (percentage >= 80 && percentage < 100) emoji = '🎉';
+	if (percentage >= 50 && percentage < 80) emoji = '👍';
+	if (percentage >= 0 && percentage < 50) emoji = '🤦';
+	if (percentage === 0) emoji = '💩';
+
+	return (
+		<>
+			<p className='result'>
+				<span>{emoji}</span> You scored <strong>{points}</strong> out of{' '}
+				{maxPoints} ({Math.ceil(percentage)}%)
+			</p>
+			<p className='highscore'>(Highscore: {highscore} points)</p>
+			<button
+				className='btn btn-ui'
+				onClick={() => dispatch({ type: 'restart' })}>
+				Restart Quiz
+			</button>
+		</>
+	);
+}
